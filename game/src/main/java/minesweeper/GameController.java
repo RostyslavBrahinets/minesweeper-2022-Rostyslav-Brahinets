a14834a447477aef7fe7b0afcff5467ffeb7ae4c
RostyslavBrahinets <rostyslav.brahinets@gmail.com>
2022-04-04 10:32:32 +0300

    Add finding mines and empty cell

--- game/src/main/java/minesweeper/GameController.java
+++ game/src/main/java/minesweeper/GameController.java
@@ -2,6 +2,8 @@ package minesweeper;
 
 import java.util.Optional;
 
+import static minesweeper.Cell.*;
+
 public class GameController {
     private final Mine mine;
     private final Flag flag;
@@ -22,7 +24,7 @@ public class GameController {
     public Optional<Cell> getCell(Coordinate coordinate) {
         Optional<Cell> cellOptional = flag.get(coordinate);
         if (cellOptional.isPresent()) {
-            if (cellOptional.get() == Cell.OPENED) {
+            if (cellOptional.get() == OPENED) {
                 return mine.get(coordinate);
             }
         }
@@ -30,11 +32,32 @@ public class GameController {
     }
 
     public void pressLeftButton(Coordinate coordinate) {
-        flag.setOpenedToCell(coordinate);
+        openCell(coordinate);
     }
 
     public void pressRightButton(Coordinate coordinate) {
         flag.toggleFlagInCell(coordinate);
+    }
+
+    private void openCell(Coordinate coordinate) {
+        Optional<Cell> flagOptional = flag.get(coordinate);
+        Optional<Cell> mineOptional = mine.get(coordinate);
+
+        if (flagOptional.isPresent() && mineOptional.isPresent()) {
+            Cell flagCell = flagOptional.get();
+            Cell mineCell = mineOptional.get();
+
+            if (flagCell == OPENED || flagCell == FLAG) {
+                return;
+            } else if (flagCell == CLOSED) {
+                if (mineCell == EMPTY || mineCell == MINE) {
+                    return;
+                } else {
+                    flag.setOpenedToCell(coordinate);
+                    return;
+                }
+            }
+        }
     }
 
     public GameState getState() {
